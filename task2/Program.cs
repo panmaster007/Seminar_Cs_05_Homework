b0d7ce7508f94f45dc9ad223e456d5f26160eec3
Igor <fullstack@naladka.su>
2023-02-13 13:15:26 +0300

    edit task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateRandomArray(int lenght, int min, int max)
+{
+	int[] array = new int[lenght];
+	Random random = new Random();
+	for (int i = 0; i < lenght; i++)
+	{
+		array[i] = random.Next(min, max +1);
+	}
+	return array;
+}
+
+void SumOddPositionNumbers(int[] array)
+{
+	int sum = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]} ");
+		if (i % 2 != 0)
+		{
+			sum = sum + array[i];
+		}
+	}
+	Console.WriteLine($" => {sum}");
+}
+
+int[] array = CreateRandomArray(4, -99, 100);
+SumOddPositionNumbers(array);
